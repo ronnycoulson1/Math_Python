481fc398c04abcac3b19d325ba06d1effbf213bc
Ronny Coulson <ronnycoulson@hotmail.com>
2025-06-19 11:53:01 -0400

    01_inverse.py

---
+++ pre_calc/01_find_inverses/01_inverses.py
@@ -0,0 +1,22 @@
+#find iverse algebraically use a function
+#f(x) = 4-5x
+
+import sympy
+
+def calcEquation():
+
+    print("Funtion: f(x)=4-5x")
+    x, y = sympy.symbols("x y")
+    f = 4 - 5 * x
+
+    equation = sympy.Eq(y, f)
+    swapped = sympy.Eq(x, f.subs(x, y))
+    solution = sympy.solve(swapped, y)
+
+    if solution:
+        inverseEquation = sympy.together(solution[0])
+        print(f"The inverse is y={inverseEquation}")
+    else:
+        print("Mo inverse")
+
+calcEquation()
